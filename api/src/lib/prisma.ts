cf5c7d685468e78f744df1dfc02129d75f9a7e5d
Fathoni M <diobok@gmail.com>
2025-11-22 06:17:01 +0700

    fix: prisma.ts

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
+import { PrismaClient } from '@prisma/client';
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaPg(pool);
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-export const prisma = new PrismaClient({ adapter });
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
